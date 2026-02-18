974b59ce93944968bca7a0053436543629069b79
Davinson Vanegas <davidsonvanegas27@gmail.com>
2026-02-18 16:37:05 -0500

    Update: Migracion a PostgreSQL y configuracion para despliegue

--- app/config.py
+++ app/config.py
@@ -2,8 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
-    # mysql+mysqlconnector://DB_user:BD_pass@BD_host/BD_name
-    SQLALCHEMY_DATABASE_URI = (
-        f"mysql+mysqlconnector://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
-    )
+    # PostgreSQL: postgresql://user:password@host/dbname
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
